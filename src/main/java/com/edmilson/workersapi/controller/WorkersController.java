63c820a1c1c166701075e0d5831eb603af91a0d2
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-03-24 17:49:25 -0300

    🚀 :rocket: [Add] WorkersController - CreateWorker

--- src/main/java/com/edmilson/workersapi/controller/WorkersController.java
+++ src/main/java/com/edmilson/workersapi/controller/WorkersController.java
@@ -1,14 +1,30 @@
 package com.edmilson.workersapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.edmilson.workersapi.dto.response.MessageResponseDTO;
+import com.edmilson.workersapi.entity.Worker;
+import com.edmilson.workersapi.service.WorkerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/api/v1/workers")
 public class WorkersController {
-    @GetMapping
-    public String getWorkers() {
-        return "API Test!";
+    private WorkerService workerService;
+
+    @Autowired
+    public void WorkerController(WorkerService workerService) {
+        this.workerService = workerService;
+    }
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createWorker(@RequestBody Worker worker) {
+        return workerService.createWorker(worker);
     }
 }
